71c874a0e676abe65f1b11e2945f07fbc19f0b08
dennisr96 <dennnisraek@gmail.com>
2021-04-07 21:17:11 +0200

    Audiogramm

--- Audio_DSP.py
+++ Audio_DSP.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import sounddevice as sd
 import scipy.io.wavfile as wavfile
+import scipy.signal as signal
 
 
 # .Wav - Manipulation
@@ -27,6 +28,11 @@ def Sinus(amp,f,fs,t):
     y = amp * np.sin(2*np.pi*f*x)
     return x, y
 
+def Sawtooth(amp,f,fs,t):
+    x = np.arange(0,t,1/fs)
+    y = amp * signal.sawtooth(2*np.pi*f*x)
+    return x, y
+
 def Speed(y, factor):
     index = np.arange(0,len(y),factor)
     return y[index[index < len(y)].astype(int)]
@@ -36,12 +42,16 @@ def Distortion(y,N):
     y = y / np.max(y)
     return y
 
+
+
+
+# Work in Progress
 def Semitone_to_Frequency(n):
     f = 440 * np.exp(n * np.log(2) / 12)
     d = n % 12
     Notes = ["A","A#","B","C","C#","D","D#","E","F","F#","G","G#"]
     Note = Notes[d]
-    print("Note {A} hat eine Frequenz von {B:.2f} Hz".format(A=Note,B=f))
+    #print("Note {A} hat eine Frequenz von {B:.2f} Hz".format(A=Note,B=f))
     return f
 
 def Frequency_to_Note(f):
@@ -50,7 +60,14 @@ def Frequency_to_Note(f):
     n = int(np.round(((12*np.log(f/55))/(np.log(2))) - 36))
     idx = n % 12
     Note = Notes[idx]
-    z = int(np.round((f / Freq[idx]) + 3))
+    z = int(np.round((f / Freq[idx]) + 3))      # WIP**
     print("Die Frequenz {A} Hz entspricht der Note {B}{C}".format(A=f,B=Note,C=z))
     return 0
 
+def ADSR(len_A,len_D,len_S,len_R,height_A,height_S):
+    curve_A = np.arange(len_A) * height_A / len_A
+    curve_D = height_A - np.arange(len_D) * (height_A - height_S) / len_D
+    curve_S = np.ones(len_S) * height_S
+    curve_R = height_S * (1 - np.arange(1, len_R + 1) / len_R)
+    curve_ADSR = np.concatenate((curve_A, curve_D, curve_S, curve_R))
+    return curve_ADSR
--- testing.py
+++ testing.py
@@ -1,20 +1,67 @@
-import Audio_DSP
-import matplotlib.pyplot as plt
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Titel: Audiogramm v3
+Autor: Dennis Räk
+HAW Hamburg -- Tontechnik 2
+"""
+
 import numpy as np
+import matplotlib.pyplot as plt
+import sounddevice as sd
 
 
+def Sinus(amp, f, fs, t):
+    x = np.arange(0, t, 1 / fs)
+    y = amp * np.sin(2 * np.pi * f * x)
+    sd.play(y, fs)
+    sd.wait()
+    return 0
 
-def ADSR(len_A,len_D,len_S,len_R,height_A,height_S):
-    curve_A = np.arange(len_A) * height_A / len_A
-    curve_D = height_A - np.arange(len_D) * (height_A - height_S) / len_D
-    curve_S = np.ones(len_S) * height_S
-    curve_R = height_S * (1 - np.arange(1, len_R + 1) / len_R)
-    curve_ADSR = np.concatenate((curve_A, curve_D, curve_S, curve_R))
-    return curve_ADSR
+def Audiogramm():
+    Ref = 0.0008  # 2kHz Referenzton
+    dB = [i for i in np.arange(-90, 90, step=5)]  # Dezibel - Range
+    dB = dB[::-1]  # Reverse Array
+    Linear = [Ref * 10 ** (i / 20) for i in dB]  # Linear Array
+    Freq_Array = [250, 500, 1000, 2000, 4000, 8000]  # Frequenz Array
+    s = 0  # Start S
+    LdB = 17
+    Amp_Array = []
+    Amp_Array_dB = []
+    while s < 6:
+        Freq = Freq_Array[s]
+        Lin = Linear[LdB]
+        Dezi = dB[LdB]
+        print("Spiele {A} Hz Sinus mit {B} dB bzw. {C:.2f}".format(A=Freq, B=Dezi, C=Lin))
+        Sinus(Lin, Freq, 48000, 3)
+        Ergebnis = input("Höher (+5dB) [+] -- Tiefer (-5dB) [-] -- [H] Hörschwelle ")
+        if Ergebnis == "+":
+            LdB -= 1
+        elif Ergebnis == "-":
+            LdB += 1
+        elif Ergebnis == "H":
+            Amp_Array.append(Linear[LdB])
+            Amp_Array_dB.append(dB[LdB])
+            LdB = 17
+            s += 1
+    return Freq_Array, Amp_Array_dB
 
-x, y = Audio_DSP.Sinus(1,3,48000,3)
-ADSR = ADSR(10,20,60,10,1,0.5)
-y = np.convolve(y,ADSR)
+def Plot(Freq_Array, Amp_Array_dB):
+    fig, ax = plt.subplots()
+    ax.plot(Freq_Array, Amp_Array_dB, marker='o', Label="Audiogramm")
+    ax.set_xscale("log")
+    ax.set_ylabel("Hörschwelle [dBHL]")
+    ax.set_xlabel("Frequenz [Hz]")
+    ax.set_title("Audiogramm")
+    ax.set_ylim(140, -20)
+    plt.xticks(Freq_Array, ["250", "500", "1K", "2K", "4K", "8K"])
+    plt.yticks([-10, 0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 120])
+    plt.grid(True)
+    plt.savefig("Audiogramm.png")
+    plt.show()
+    return 0
 
-plt.plot(y)
-plt.show()
+
+Freq_Array, Amp_Array_dB = Audiogramm()
+print(Amp_Array_dB)
+Plot(Freq_Array, Amp_Array_dB)
