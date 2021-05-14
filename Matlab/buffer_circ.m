26caf9435509ccfa595d110ce497f737e3c7711c
DennisR96 <81929072+DennisR96@users.noreply.github.com>
2021-05-14 14:03:23 +0200

    Final v1

--- Matlab/buffer_circ.m
+++ Matlab/buffer_circ.m
@@ -2,8 +2,8 @@ function [y, buffer] = buffer_circ(x,buffer,delay,n)
 len = length(buffer);                   % Length Buffer
 indexC = mod(n-1,len) + 1;              % Circular Index
 indexD = mod(n-delay-1,len) + 1;        % Delay Index
-y = buffer(indexD,1);                   % Output
-buffer(indexC,1) = x;                   % Buffer 
+y = buffer(1,indexD);                   % Output
+buffer(1,indexC) = x;                   % Buffer 
 end
 
 % %%Example
@@ -11,10 +11,9 @@ end
 % x = [1,0,0,0,0,0,0]';               % Input: (N,1)
 % N = length(x);                      % Array Size
 % N_b = 6;                            % Buffer Size
-% buffer = zeros(N_b,1);              % Buffer
+% buffer = zeros(1,N_b);              % Buffer
 % delay = 3;                          % Delay
 % y = zeros(N,1);                     % Output: (N,1)
 % for n = 1:N
 %     [y(n,1),buffer] = buffer_circ(x(n,1),buffer,delay,n);
 % end 
-
