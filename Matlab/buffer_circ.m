9a123e7cf982c84f1f45b67fa08647084102f191
DennisR96 <81929072+DennisR96@users.noreply.github.com>
2021-05-16 01:03:34 +0200

    Update buffer_circ.m

--- Matlab/buffer_circ.m
+++ Matlab/buffer_circ.m
@@ -3,6 +3,6 @@ function [out, buffer] = buffer_circ(in,buffer,delay,n)
 len = length(buffer);                   % Length Buffer
 indexC = mod(n-1,len) + 1;              % Circular Index
 indexD = mod(n-delay-1,len) + 1;        % Delay Index
-out = buffer(indexD,1);                   % Output
+out = buffer(indexD,1);                 % Output
 buffer(indexC,1) = x;                   % Buffer 
 end
